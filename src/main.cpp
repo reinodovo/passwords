f2cf20948375a69de156c88189a69f7ac8a35094
Pedro Sousa <pedroteosousa@gmail.com>
2023-10-25 21:55:53 -0300

    Use new BombProtocol features

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 #include <PuzzleModule.h>
 #include <Rules.h>
+#include <utils/Button.h>
 
 #include "display.h"
-#include "button.h"
 
 const int redPin = 27, greenPin = 14;
 const int SEGMENTS = 7;
+const int SUBMIT_BUTTON_PIN = 26;
 
 WordList words;
 const int OPTIONS = 6;
@@ -15,7 +16,24 @@ struct PositionOptions {
     int selected;
 } position[WORD_LENGTH];
 
-Button buttons[COLS * ROWS];
+const int BUTTON_COLS = 5;
+const int BUTTON_COL_PINS[BUTTON_COLS] = {34, 39, 36, 35, 32};
+const int BUTTON_ROWS = 2;
+const int BUTTON_ROW_PINS[BUTTON_ROWS] = {33, 25};
+
+Button buttons[BUTTON_COLS][BUTTON_ROWS];
+Button submitButton;
+
+struct SelectButtonFunction {
+    int col, row;
+    SelectButtonFunction(int col, int row): col(col), row(row) {}
+    void operator() (State state) {
+        if (state != State::PRESSED) return;
+        int offset = (row == 0) ? -1 : 1;
+        position[col].selected = (position[col].selected + offset + OPTIONS) % OPTIONS;
+        Display::word[col] = position[col].letters[position[col].selected];
+    }
+};
 
 int answer;
 
@@ -67,17 +85,32 @@ void generate_options() {
     }
 }
 
-void button_pressed(ButtonState state, int col, int row) {
-    if (state != ButtonState::Pressed) return;
-    int offset = (row == 0) ? -1 : 1;
-    position[col].selected = (position[col].selected + offset + OPTIONS) % OPTIONS;
-    Display::word[col] = position[col].letters[position[col].selected];
+void onSeed(int seed) {
+    words = generate_words(seed);
+    generate_options();
+}
+
+void submit(State state) {
+    if (state != State::PRESSED) return;
+    bool correct = true;
+    for (int i = 0; i < WORD_LENGTH; i++) {
+        if (position[i].letters[position[i].selected] != words[answer][i]) {
+            correct = false;
+            break;
+        }
+    }
+    if (correct) {
+        PuzzleModule::solve();
+    } else {
+        PuzzleModule::strike();
+    }
 }
 
 void setup() {
     pinMode(CLOCK, OUTPUT);
     pinMode(STORE, OUTPUT);
     pinMode(SER, OUTPUT);
+    pinMode(SUBMIT_BUTTON_PIN, INPUT);
 
     for (int i = 0; i < DIGITS; i++) {
         pinMode(DIGIT_PINS[i], OUTPUT);
@@ -87,42 +120,40 @@ void setup() {
     PuzzleModule::StatusLight statusLight;
     statusLight.init(redPin, greenPin);
 
-    if (!PuzzleModule::init(statusLight))
-    {
+    if (!PuzzleModule::init(statusLight, onSeed))
         ESP.restart();
+
+    for (int i = 0; i < BUTTON_COLS; i++) {
+        pinMode(BUTTON_COL_PINS[i], INPUT);
+    }
+    for (int i = 0; i < BUTTON_ROWS; i++) {
+        pinMode(BUTTON_ROW_PINS[i], OUTPUT);
+        digitalWrite(BUTTON_ROW_PINS[i], LOW);
     }
 
-    for (int i = 0; i < COLS; i++) {
-        pinMode(COL_PINS[i], INPUT);
-    }
-    for (int i = 0; i < ROWS; i++) {
-        pinMode(ROW_PINS[i], OUTPUT);
-        digitalWrite(ROW_PINS[i], LOW);
-    }
-    for (int i = 0; i < COLS; i++) {
-        for (int j = 0; j < ROWS; j++) {
-            buttons[i * ROWS + j].init(i, j, button_pressed);
+    for (int i = 0; i < BUTTON_COLS; i++) {
+        for (int j = 0; j < BUTTON_ROWS; j++) {
+            buttons[i][j] = Button(BUTTON_COL_PINS[i]);
+            buttons[i][j].onStateChange = SelectButtonFunction(i, j);
         }
     }
+
+    submitButton = Button(SUBMIT_BUTTON_PIN);
+    submitButton.onStateChange = submit;
 
     Display::init();
 }
 
-unsigned long lastAttempt = 0;
-unsigned long attemptGap = 100;
-
 void loop() {
   PuzzleModule::update();
-  if (words.size() == 0 && millis() - lastAttempt > attemptGap) {
-    lastAttempt = millis();
-    PuzzleModule::withBombInfo([](BombInfo info) {
-      words = generate_words(info.seed);
-      generate_options();
-    });
-    return;
-  }
+  if (words.size() == 0) return;
   Display::update();
-  for (int i = 0; i < ROWS * COLS; i++) {
-    buttons[i].update();
+  for (int i = 0; i < BUTTON_ROWS; i++) {
+    digitalWrite(BUTTON_ROW_PINS[(i - 1 + BUTTON_ROWS) % BUTTON_ROWS], LOW);
+    digitalWrite(BUTTON_ROW_PINS[i], HIGH);
+    for (int j = 0; j < BUTTON_COLS; j++) {
+      buttons[j][i].update();
+    }
   }
+  submitButton.update();
 }
